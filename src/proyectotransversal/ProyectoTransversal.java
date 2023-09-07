087f1e14cabed32b536490d674525746449ed821
EmitoStuer <stuer.87@gmail.com>
2023-09-07 12:11:42 -0300

    si se puede

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -15,7 +15,7 @@ public class ProyectoTransversal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("VAMOS QUE SE PUEDE!");
     }
     
 }
