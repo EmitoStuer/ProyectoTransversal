7b2fe3a1252b91023e432ad6b9037ad745bb6d14
EmitoStuer <stuer.87@gmail.com>
2023-09-07 12:24:54 -0300

    limpioPantalla

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -7,13 +7,7 @@ public class ProyectoTransversal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("VAMOS QUE SE PUEDE!");
-  
-        System.out.println("Fer");
-        
-        System.out.println("Mauri");
 
-        System.out.println("Alex");
     }
     
 }
