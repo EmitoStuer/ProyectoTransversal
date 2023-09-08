7448c76e3bbc450a7b61cd8e9e3f18327c998696
AlexM4xi <alexesquivel136@gmail.com>
2023-09-08 13:03:10 -0300

    Conecion

--- src/proyectotransversal/AccesoDatos/Conexion.java
+++ src/proyectotransversal/AccesoDatos/Conexion.java
@@ -1,11 +1,32 @@
 package proyectotransversal.AccesoDatos;
 import java.sql.*;
+import javax.swing.JOptionPane;
 public class Conexion {
-    private final String host="jdbc:mysql://localhost/proyectotransversal";
-    private final String user="root";
-    private final String pwd="";
+    private static String url="jdbc:mariadb://localhost:3306/proyectotransversal";
+    private static String user="root";
+    private static String pwd="";
+    private static Conexion conexion=null;
     
-    private Connection conection=null;
+    private Conexion(){
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null,"Error carga de driver: "+ e.getMessage());
+        }
+    }
     
-    
+    private static Connection getConexion(){
+        Connection con=null;
+        if(conexion == null){
+           conexion= new Conexion();
+        }
+        try{
+            // Setup the connection with the DB
+            con=DriverManager.getConnection(url,user,pwd);
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion ");
+        }
+        
+        return con;
+    }
 }
