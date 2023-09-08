9c1b2655b529e6f04ad74603e1fee9840538d0d4
AlexM4xi <alexesquivel136@gmail.com>
2023-09-08 10:46:40 -0300

    actualixado1

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -7,6 +7,7 @@ public class ProyectoTransversal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("JAshcabc");
 
     }
     
