09f311aca3e22401c86f78c7e23b1220fea205e2
EmitoStuer <stuer.87@gmail.com>
2023-09-08 11:57:29 -0300

    borrado

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -7,7 +7,6 @@ public class ProyectoTransversal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("JAshcabc");
 
     }
     
