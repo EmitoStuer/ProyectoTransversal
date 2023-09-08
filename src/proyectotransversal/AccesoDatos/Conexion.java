3b49806cbe53f873dd4e7a3cbf14c69cc41bb364
AlexM4xi <alexesquivel136@gmail.com>
2023-09-08 12:19:11 -0300

    claseConexion

---
+++ src/proyectotransversal/AccesoDatos/Conexion.java
@@ -0,0 +1,11 @@
+package proyectotransversal.AccesoDatos;
+import java.sql.*;
+public class Conexion {
+    private final String host="jdbc:mysql://localhost/proyectotransversal";
+    private final String user="root";
+    private final String pwd="";
+    
+    private Connection conection=null;
+    
+    
+}
