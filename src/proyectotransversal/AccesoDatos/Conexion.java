f224abd5bbf12cb0fdbf483bb2676d8516cec382
FerMSz <fermsz.89@gmail.com>
2023-09-11 10:36:19 -0300

    4

--- src/proyectotransversal/AccesoDatos/Conexion.java
+++ src/proyectotransversal/AccesoDatos/Conexion.java
@@ -2,9 +2,9 @@ package proyectotransversal.AccesoDatos;
 import java.sql.*;
 import javax.swing.JOptionPane;
 public class Conexion {
-    private static String url="jdbc:mariadb://localhost:3306/proyectotransversal";
-    private static String user="root";
-    private static String pwd="";
+    private static final String URL="jdbc:mariadb://localhost:3306/proyectotransversal";
+    private static final String USER="root";
+    private static final String PWD="";
     private static Conexion conexion=null;
     
     private Conexion(){
@@ -22,7 +22,7 @@ public class Conexion {
         }
         try{
             // Setup the connection with the DB
-            con=DriverManager.getConnection(url,user,pwd);
+            con=DriverManager.getConnection(URL,USER,PWD);
         }catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error de conexion ");
         }
